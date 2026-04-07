6e61d43088bf4b24438b51abdbacf8ed15907709
Tommy <tom6573637618@gmail.com>
2026-04-07 11:48:32 +0700

    fix(product): remove remaining uom_type assignments in UpdateUnitCommandHandler

--- Services/Product/CRM.Product.Application/Features/Products/Commands/UpdateAndDeleteUnitHandlers.cs
+++ Services/Product/CRM.Product.Application/Features/Products/Commands/UpdateAndDeleteUnitHandlers.cs
@@ -21,7 +21,6 @@ namespace CRM.Product.Application.Features.Products.Commands
 
             if (request.Code != null) entity.code = request.Code;
             if (request.Name != null) entity.name = request.Name;
-            if (request.UomType != null) entity.uom_type = request.UomType;
 
             await _repository.UpdateAsync(entity);
             return true;
